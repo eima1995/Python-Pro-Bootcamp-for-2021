ba35620f84c5fb3798d39769b8ad6143349ee61e
eimaRR <eimantas.bendorius@royaltyrange.com>
2020-11-26 18:48:29 +0200

    complete task CoffeeMachineOPP

--- CoffeeMachineOOP/README.md
+++ CoffeeMachineOOP/README.md
@@ -1,5 +1,5 @@
 # Coffee Machine Program OOP
 
-#[Requirements FILE](CoffeeMachine/Coffee+Machine+Program+Requirements.pdf)
+#[Requirements FILE](https://github.com/eima1995/Python-Pro-Bootcamp-for-2021/blob/main/CoffeeMachine/Coffee+Machine+Program+Requirements.pdf)
 
 #[Solution](https://repl.it/@appbrewery/coffee-machine-final#main.py)
--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -1,7 +1,21 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
+import sys
 
 if __name__ == '__main__':
-    pass
+    menu = Menu()
+    coffee_machine = CoffeeMaker()
+    money_machine = MoneyMachine()
+    while True:
+        choice = input("What would you like? (espresso/latte/cappuccino):")
+        if choice == 'off':
+            sys.exit(0)
+        elif choice == 'report':
+            coffee_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+            if drink:
+                if coffee_machine.is_resource_sufficient(drink):
+                    if money_machine.make_payment(drink.cost):
+                        coffee_machine.make_coffee(drink)
