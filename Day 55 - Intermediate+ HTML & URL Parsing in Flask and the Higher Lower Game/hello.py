423446eff7ad9830c7dee8a453e9c6f708b76842
eimaRR <eimantas.bendorius@royaltyrange.com>
2020-11-27 20:26:53 +0200

    day 55 done flask task

--- Day 55 - Intermediate+ HTML & URL Parsing in Flask and the Higher Lower Game/hello.py
+++ Day 55 - Intermediate+ HTML & URL Parsing in Flask and the Higher Lower Game/hello.py
@@ -1,37 +1,27 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 
-def make_bold(function):
-    def wrapper():
-        return "<b>" + function() + "</b>"
-    return wrapper
-
-
-def make_emphasis(function):
-    def wrapper():
-        return "<em>" + function() + "</em>"
-    return wrapper
-
-
-def make_underlined(function):
-    def wrapper():
-        return "<u>" + function() + "</u>"
-    return wrapper
-
-
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    generate_random_num()
+    return "<h1>Guess a number between 0 and 9<h1>" \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"/>'
 
 
-@app.route("/bye")
-@make_bold
-# @make_emphasis
-# @make_underlined
-def bye():
-    return 'Bye'
+@app.route("/<int:num>")
+def guess_number(num):
+    if num == random_number:
+        return "<h1>just right:</h1> " \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>?"
+    elif num < random_number:
+        return "<h1>is too low:</h1>" \
+            "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>?"
+    else:
+        return "<h1>is too high:</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>?"
 
 
 @app.route("/usernames/<name>")
@@ -39,5 +29,11 @@ def greetings(name):
     return f"Hello {name}"
 
 
+def generate_random_num():
+    global random_number
+    random_number = random.randint(0, 9)
+
+
+random_number = 0
 if __name__ == '__main__':
     app.run(debug=True)
